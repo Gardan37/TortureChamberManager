0a28c91bfa1bd3fadc854a8647e89f8e2cf079e6
Aku Seppälä <akusep@gmail.com>
2018-09-11 17:06:47 +0300

    Added the start game button functionality

--- Assets/Scripts/SimpleSceneSwitch.cs
+++ Assets/Scripts/SimpleSceneSwitch.cs
@@ -2,37 +2,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Kapu.SaamiRunner
-{
 	public class SimpleSceneSwitch : MonoBehaviour
 	{
-		private static Stack<string> sceneStack;
 
-		private void Awake()
-		{
-			if (sceneStack == null || sceneStack.Count == 0)
-			{
-				sceneStack = new Stack<string>();
-				sceneStack.Push("s_Saami_Start");
-			}
-		}
-		
-		public void OpenRewardsScene()
-		{
-			sceneStack.Push(SceneManager.GetActiveScene().name);
-			SceneManager.LoadScene("s_Saami_Rewards");
-		}
-		
 		public void OpenStartScene()
 		{
-			sceneStack.Push(SceneManager.GetActiveScene().name);
-			SceneManager.LoadScene("s_Saami_Start");
+			SceneManager.LoadScene("MainMenu-001");
 		}
 
 		public void OpenGameplayScene()
 		{
-			sceneStack.Push(SceneManager.GetActiveScene().name);
-			SceneManager.LoadScene("s_Saami_Gameplay");
+			SceneManager.LoadScene("Gameplay-001");
 		}
 //		public void OpenGameplayScene2()
 //		{
@@ -40,10 +20,9 @@ namespace Kapu.SaamiRunner
 //			SceneManager.LoadScene("s_Saami_Gameplay_2");
 //		}
 
-		public void OpenPreviousScene()
+/*		public void OpenPreviousScene()
 		{
-			var prevSceneName = sceneStack.Pop();
 			SceneManager.LoadScene(prevSceneName);
-		}
+		}*/
+
 	}
-}
