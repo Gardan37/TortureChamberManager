2fe6f65ddfcc7fdf715e43094a109e83c5c06fc3
Aku Seppälä <akusep@gmail.com>
2018-09-11 16:41:46 +0300

    All kinds of small things and sounds

---
+++ Assets/Scripts/SimpleSceneSwitch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Kapu.SaamiRunner
+{
+	public class SimpleSceneSwitch : MonoBehaviour
+	{
+		private static Stack<string> sceneStack;
+
+		private void Awake()
+		{
+			if (sceneStack == null || sceneStack.Count == 0)
+			{
+				sceneStack = new Stack<string>();
+				sceneStack.Push("s_Saami_Start");
+			}
+		}
+		
+		public void OpenRewardsScene()
+		{
+			sceneStack.Push(SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene("s_Saami_Rewards");
+		}
+		
+		public void OpenStartScene()
+		{
+			sceneStack.Push(SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene("s_Saami_Start");
+		}
+
+		public void OpenGameplayScene()
+		{
+			sceneStack.Push(SceneManager.GetActiveScene().name);
+			SceneManager.LoadScene("s_Saami_Gameplay");
+		}
+//		public void OpenGameplayScene2()
+//		{
+//			sceneStack.Push(SceneManager.GetActiveScene().name);
+//			SceneManager.LoadScene("s_Saami_Gameplay_2");
+//		}
+
+		public void OpenPreviousScene()
+		{
+			var prevSceneName = sceneStack.Pop();
+			SceneManager.LoadScene(prevSceneName);
+		}
+	}
+}
