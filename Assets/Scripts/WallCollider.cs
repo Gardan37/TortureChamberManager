5e0987a9a77da0b47b48bf6a5d2bd05cbe1bea74
Aku Seppälä <akusep@gmail.com>
2018-09-12 11:21:04 +0300

    New camera system

--- Assets/Scripts/WallCollider.cs
+++ Assets/Scripts/WallCollider.cs
@@ -6,11 +6,20 @@ public class WallCollider : MonoBehaviour
 {
     public GameObject player;
     public string wallName;
+    AudioSource audioData;
 
+ void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    //    audioData.Play(0);
+    //    Debug.Log("started");
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other == player.GetComponent<Collider2D>())
         {
+        	        audioData.Play(0);
+
             player.SendMessage("WallHit", wallName);
         }
 
