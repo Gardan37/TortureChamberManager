240556ecb7be671e310420685d889da7cc353650
Aku Seppälä <akusep@gmail.com>
2018-09-12 15:28:44 +0300

    Added sounds

--- Assets/Scripts/TrapCollider.cs
+++ Assets/Scripts/TrapCollider.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class TrapCollider : MonoBehaviour
 {
     public int hitForce = 0;
+    AudioSource audioData;
 
     public GameObject player;
-
+ void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.SendMessage("PlayerHit", hitForce);
+            audioData.Play(0);
+
         }
     }
 }
